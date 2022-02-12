da7547cbebf4c2954805a79847e0a1579e4c9678
PhilipVigus <philipvigus@yahoo.co.uk>
2022-02-12 15:12:37 +0000

    Add a failing test

--- src/test/java/com/philvigus/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/philvigus/urlshortener/UrlShortenerApplicationTests.java
@@ -3,11 +3,18 @@ package com.philvigus.urlshortener;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void failingTest() {
+        assertEquals(true, false);
     }
 
 }
