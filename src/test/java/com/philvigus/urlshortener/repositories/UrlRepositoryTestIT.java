3f7cdd3e54e289c27e33e0fe9043f7046a6969ab
PhilipVigus <philipvigus@yahoo.co.uk>
2022-02-15 19:30:00 +0000

    Add test configuration properties

--- src/test/java/com/philvigus/urlshortener/repositories/UrlRepositoryTestIT.java
+++ src/test/java/com/philvigus/urlshortener/repositories/UrlRepositoryTestIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -12,6 +13,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 @DataJpaTest
 class UrlRepositoryTestIT {
   @Autowired UrlRepository urlRepository;
