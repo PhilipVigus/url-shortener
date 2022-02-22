d2eb09d45cd37e62b004a2d1aeddb63ca62c5e9c
PhilipVigus <philipvigus@yahoo.co.uk>
2022-02-22 17:20:59 +0000

    Add missing test profile

--- src/test/java/com/philvigus/urlshortener/controllers/HomeControllerIntegrationTest.java
+++ src/test/java/com/philvigus/urlshortener/controllers/HomeControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class HomeControllerIntegrationTest {
   @Autowired private WebApplicationContext context;
