f48e3187041ca48e172077f6808586a24e13daa2
PhilipVigus <philipvigus@yahoo.co.uk>
2022-02-26 12:09:40 +0000

    Set the homepage as the landing page for unauthed users

--- src/main/java/com/philvigus/urlshortener/controllers/HomeController.java
+++ src/main/java/com/philvigus/urlshortener/controllers/HomeController.java
@@ -1,8 +1,6 @@
 package com.philvigus.urlshortener.controllers;
 
-import com.philvigus.urlshortener.model.User;
 import com.philvigus.urlshortener.services.UserService;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,9 +13,7 @@ public class HomeController {
   }
 
   @RequestMapping("")
-  public String view(@AuthenticationPrincipal User activeUser) {
-    System.out.println("###################");
-    System.out.println(activeUser);
+  public String view() {
     return "home";
   }
 }
--- src/main/java/com/philvigus/urlshortener/security/WebSecurityConfig.java
+++ src/main/java/com/philvigus/urlshortener/security/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/")
+        .antMatchers("/dashboard")
         .authenticated()
         .anyRequest()
         .permitAll()
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .formLogin()
         .loginPage("/login")
         .usernameParameter("username")
-        .defaultSuccessUrl("/")
+        .defaultSuccessUrl("/dashboard")
         .permitAll()
         .and()
         .logout()
