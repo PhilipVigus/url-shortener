87242f10e2575e41d4580f6a2b92794f9589e814
PhilipVigus <philipvigus@yahoo.co.uk>
2022-02-28 17:18:49 +0000

    Add missing Repository annotation

--- src/main/java/com/philvigus/urlshortener/repositories/UserRepository.java
+++ src/main/java/com/philvigus/urlshortener/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.philvigus.urlshortener.repositories;
 import com.philvigus.urlshortener.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
   @Query("SELECT u FROM User u WHERE u.username = ?1")
   User findByUsername(String username);
