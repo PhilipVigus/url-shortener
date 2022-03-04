d1ecf75ebbfdde44f6c3107a3be3373728bdc100
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-04 16:35:57 +0000

    Remove unneeded test annotation

--- src/test/java/com/philvigus/urlshortener/controllers/DashboardControllerTest.java
+++ src/test/java/com/philvigus/urlshortener/controllers/DashboardControllerTest.java
@@ -3,15 +3,12 @@ package com.philvigus.urlshortener.controllers;
 import com.philvigus.urlshortener.model.Url;
 import com.philvigus.urlshortener.repositories.UrlRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -22,8 +19,6 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Disabled
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @SpringBootTest
 class DashboardControllerTest {
