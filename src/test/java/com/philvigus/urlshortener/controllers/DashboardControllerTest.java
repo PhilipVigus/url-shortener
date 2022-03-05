dc1316fb28fe6951ab45dc90d4fd6db431a3ad20
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-05 12:16:16 +0000

    Allow users to add URLs

--- src/test/java/com/philvigus/urlshortener/controllers/DashboardControllerTest.java
+++ src/test/java/com/philvigus/urlshortener/controllers/DashboardControllerTest.java
@@ -1,22 +1,27 @@
 package com.philvigus.urlshortener.controllers;
 
 import com.philvigus.urlshortener.model.Url;
-import com.philvigus.urlshortener.repositories.UrlRepository;
+import com.philvigus.urlshortener.services.UrlService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.List;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test")
@@ -24,7 +29,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class DashboardControllerTest {
   @Autowired private WebApplicationContext context;
 
-  @Autowired private UrlRepository urlRepository;
+  @Autowired private UrlService urlService;
 
   private MockMvc mvc;
 
@@ -37,12 +42,13 @@ class DashboardControllerTest {
   @Sql("classpath:createUserWithUrl.sql")
   @Sql(scripts = "classpath:clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
   @Test
-  public void anAuthedUserCanAccessTheHomepage() throws Exception {
+  public void anAuthedUserCanAccessTheDashboard() throws Exception {
+
     mvc.perform(get("/dashboard")).andExpect(view().name("dashboard")).andExpect(status().isOk());
   }
 
   @Test
-  public void aGuestUserCannotAccessTheHomepage() throws Exception {
+  public void aGuestUserCannotAccessTheDashboard() throws Exception {
     mvc.perform(get("/dashboard")).andExpect(status().is3xxRedirection());
   }
 
@@ -51,10 +57,45 @@ class DashboardControllerTest {
   @Sql(scripts = "classpath:clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
   @Test
   public void AnAuthedUserSeesTheirUrls() throws Exception {
-    List<Url> urls = (List<Url>) urlRepository.findAll();
+    mvc.perform(get("/dashboard")).andExpect(model().attributeExists("urls"));
+
+    Set<Url> urls = urlService.findAll();
+
+    assertEquals(1, urls.size());
+  }
+
+  @WithMockUser(username = "phil", password = "password")
+  @Sql("classpath:createUserWithoutUrl.sql")
+  @Sql(scripts = "classpath:clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+  @Test
+  public void AnAuthedUserWithNoUrlsSeesNoUrls() throws Exception {
+    mvc.perform(get("/dashboard")).andExpect(model().attributeExists("urls"));
+
+    Set<Url> urls = urlService.findAll();
+
+    assertEquals(0, urls.size());
+  }
+
+  @WithMockUser(username = "phil", password = "password")
+  @Sql("classpath:createUserWithoutUrl.sql")
+  @Sql(scripts = "classpath:clean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+  @Test
+  public void AnAuthedUserCanCreateAUrl() throws Exception {
+    final String FULL_URL = "https://www.google.com";
+    mvc.perform(
+            post("/dashboard")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("fullUrl", FULL_URL))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(MockMvcResultMatchers.view().name("redirect:/dashboard"));
+
+    Set<Url> urls = urlService.findAll();
+
+    assertEquals(1, urls.size());
 
     Url url = urls.stream().findFirst().get();
 
-    mvc.perform(get("/dashboard")).andExpect(model().attributeExists("urls"));
+    assertEquals(FULL_URL, url.getFullUrl());
   }
 }
