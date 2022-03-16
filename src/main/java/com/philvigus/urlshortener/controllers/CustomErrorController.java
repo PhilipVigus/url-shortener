f31a77dde3af56d34b6dd0c6e5c1047245c9c3f1
PhilipVigus <philipvigus@yahoo.co.uk>
2022-03-16 18:26:08 +0000

    Refactor method

--- src/main/java/com/philvigus/urlshortener/controllers/CustomErrorController.java
+++ src/main/java/com/philvigus/urlshortener/controllers/CustomErrorController.java
@@ -14,22 +14,24 @@ public class CustomErrorController implements ErrorController {
   public String handleError(final HttpServletRequest request) {
     final Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
+    String returnView = "error/error";
+
     if (status != null) {
       final Integer statusCode = Integer.valueOf(status.toString());
 
       if (statusCode == HttpStatus.NOT_FOUND.value()) {
-        return "error/not-found";
+        returnView = "error/not-found";
       }
 
       if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-        return "error/server";
+        returnView = "error/server";
       }
 
       if (statusCode == HttpStatus.FORBIDDEN.value()) {
-        return "error/forbidden";
+        returnView = "error/forbidden";
       }
     }
 
-    return "error/error";
+    return returnView;
   }
 }
